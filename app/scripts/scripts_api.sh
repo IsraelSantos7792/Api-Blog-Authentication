555cb7702583a14e22098098cc16a6f96d03f4cd
IsraelSantos7792 <israelqueren1805@gmail.com>
2021-12-02 15:38:41 -0300

    ShellScriptComplete

--- app/scripts/scripts_api.sh
+++ app/scripts/scripts_api.sh
@@ -4,6 +4,7 @@
 
 API_URL='http://localhost:3000/api/auth'
 
+# password mínimo de 8 caracteres 
 BODY='{"email":"teste@teste.com","password":"12345678","password_confirmation":"12345678"}'
 
 # Criando um novo usuário 
@@ -17,3 +18,76 @@ mk_req () {
 
 mk_req
 
+# Possibilidades da Api
+# Auth
+#   Section
+#      - Entrar na sessão 
+#      - Sair sessão
+     
+#   Confimation
+#      - Cofirmação e-mail cadastrado / ou ao atualziar a senha.
+     
+#   Password
+#      - Esqueci a senha 
+#      - Atualizar a senha
+#      - Resetar a senha
+     
+#   Validate
+#      - Validar Torken usuáio logado
+
+#   Users: 
+#      - Criar um novo usuário
+#      - Atualizar usuário
+#      - Atualizar email usuário
+#      - Deletar usuário
+     
+# Articles
+#   - Criar / Read / Atualizar / Deletar
+
+
+# #Atualizar usuário 
+#     method:Put
+#     BODY='{"name": "","nickname": ""}'
+#     API_URL='http://localhost:3000/api/auth'
+#     HEADERS='{'Content-Type: application/json','access-token':'','uid':'','client':''}'
+
+# #Deletar usuário 
+#     method:Del
+#     API_URL='http://localhost:3000/api/auth'
+#     HEADERS='{'Content-Type: application/json','access-token':'','uid':'','client':''}'
+
+# #Sign_in 
+#     method:Post
+#     API_URL1='http://localhost:3000/api/auth/sign_in'
+#     BODY='{"email": "","password": ""}'
+#     HEADERS='{'Content-Type: application/json'}'
+
+# #Sign_out 
+#     method:Del
+#     API_URL1='http://localhost:3000/api/auth/sign_out'
+#     HEADERS='{'Content-Type: application/json','access-token':'','uid':'','client':''}'
+
+# #Validar Token do usuário
+#     method:Get
+#     API_URL1='http://localhost:3000/api/auth/validate_token'
+#     HEADERS='{'Content-Type: application/json','access-token':'','uid':'','client':''}'
+
+# #Esqueceu a senha
+#     method:Post
+#     API_URL1='http://localhost:3000/api/auth/password'
+#     BODY='{"email": ""}'
+#     HEADERS='{'Content-Type: application/json'}'
+
+# #Atualizar a senha 
+#     method:Put
+#     API_URL1='http://localhost:3000/api/auth'
+#     BODY='{"current_password": "","password": "","password-confirmation": ""}'
+#     HEADERS='{'Content-Type: application/json','access-token':'','uid':'','client':''}'
+
+
+# Artigos (Criar, ler, deletar e atualizar)
+#     method:Post / Get / Put / Del
+#     API_URL1='http://localhost:3000/api/articles'
+#     BODY='{"title": "","body": ""}' <- usado somento no Post
+#     HEADERS='{'Content-Type: application/json','access-token':'','uid':'','client':''}'  <- usado em todos
+#     Obs: Para Update e Delete de artigos necessário /id no final da url
